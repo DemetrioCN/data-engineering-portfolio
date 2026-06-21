d389587b97b3ab7032ccb91a69950ae10b060e91
Demetrio <dem9411@gmail.com>
2026-06-20 22:25:24 -0600

    Update:
    Improve customer master model anda warehouse

---
+++ p1-cpg-logistic-distribution/1_simulator/customer_master_model/config/customers_params.py
@@ -0,0 +1,40 @@
+"""
+Global settings for the CUSTOMER MASTER pipeline.
+Edit this file to configure timeouts, servers, and output format, brands and chanfe regional warehouse mapping.
+"""
+from pathlib import Path
+
+# Overpass API: it try with two options. 
+OVERPASS_SERVERS = [
+    "https://overpass-api.de/api/interpreter",
+    "https://overpass.kumi.systems/api/interpreter",
+]
+
+APP_NAME = "customer_base_generator"
+MAX_RETRIES = 3
+TIMEOUT = 120          # seconds per request
+RETRY_WAIT = 10        # seconds between retries
+RATE_LIMIT_WAIT = 60   # seconds to wait on HTTP 429
+
+# Pipeline config
+SLEEP_BETWEEN_BRANDS = 5   # seconds between brand queries
+GEOFENCE_RADIUS_M = 150    # metres
+
+# Output
+OUTPUT_FORMAT = "csv"
+OUTPUT_DIR = Path("../data/")
+
+
+# Segment distribution to classify clients with loyalty
+SEGMENT_DISTRIBUTION = {
+    "bronze": 0.34,
+    "silver": 0.45,
+    "gold":   0.21,
+}
+
+# BRANDS dictionary of store and its wikidata id. It can find the wikidata id here https://nsi.guide/index.html?t=* 
+BRANDS = {
+    "Walmart": "Q483551",
+    "Soriana": "Q735562",
+    "Chedraui": "Q2961952"
+}
---
+++ p1-cpg-logistic-distribution/1_simulator/customer_master_model/customer_master_generator.py
@@ -0,0 +1,45 @@
+"""
+Generator for mock data to create a base client catalog classify by loyalty.
+"""
+
+import logging
+from config.customers_params import BRANDS, OUTPUT_DIR, OUTPUT_FORMAT
+from pipeline.customer_extractor import extract
+from pipeline.customer_transformer import transform
+from pipeline.customer_loader import load
+from datetime import datetime
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)-8s | %(name)s – %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+
+def run_customer_master() -> int:
+    """Execute the full ETL pipeline. Returns 0 on success, 1 on failure."""
+    logger.info("Starting CUSTOMER MASTER pipeline")
+
+    # Extract
+    df_raw = extract(brands=BRANDS)
+    if df_raw.empty:
+        logger.error("Extraction returned no data. Aborting pipeline.")
+        return 1
+
+    # Transform
+    df_transformed, df_warehouse = transform(df_raw)
+
+    today    = datetime.now()
+    date_str = today.strftime("%Y%m%d")
+
+    # Load
+    customer_out_path = load(df_transformed, filename=f'{date_str}_customer_master',output_dir=OUTPUT_DIR / "customer_master", fmt=OUTPUT_FORMAT)
+    warehouse_out_path = load(df_warehouse, filename=f'{date_str}_warehouse_master' ,output_dir=OUTPUT_DIR / "warehouse_master", fmt=OUTPUT_FORMAT)
+
+    logger.info("Done → %s | %s", customer_out_path, warehouse_out_path)
+    return 0
+
+
+if __name__ == "__main__":
+    run_customer_master()
